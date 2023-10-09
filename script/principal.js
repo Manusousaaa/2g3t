3a306095522164f905d9331c2ad71869bf7ff38a
Tokikawa_06 <106184151+Emytoky@users.noreply.github.com>
2023-10-09 11:31:20 -0300

    Add files via upload

--- script/principal.js
+++ script/principal.js
@@ -1,5 +1,5 @@
 escrever = (msg) => alert(msg);
-soma = (a,b) => a + b;
+soma = (a,b) => Number(a) + Number(b);
 sub = (a,b) => a - b;
 div = (a,b) => a / b;
 mult = (a,b) => a * b;
@@ -12,5 +12,87 @@ eq2grau = (a,b,c) => {
     return "x1 " + div(soma(-b,raiz(delta)),mult(2,a)) + 
            "x2 " + div(sub(-b,raiz(delta)),mult(2,a));
 }
-// x2 + 3x -8 = 0
-escrever(eq2grau(1,3,-8));
+let a = "";
+let b = "";
+let op = "";
+let valor = "";
+let tem_ponto = false;
+let desligada = false;
+
+function raiz_quadrada(){
+    mostra_resultado(raiz(valor));
+    valor = "";
+}
+
+function porcentagem(){
+    if(op == "mult"){
+        mostra_resultado(div(mult(a,valor),100));
+        a = "";
+        valor = "";
+    }
+}
+
+function zerar(){
+    if(desligada) return;
+    a = "";
+    b = "";
+    op = "";
+    valor = "";
+    tem_ponto = false;
+    mostra_resultado(0);
+}
+
+function desligar(){
+    if(desligada){
+        desligada = false;
+        zerar();
+    }else{
+        zerar();
+        mostra_resultado("");
+        desligada = true;
+    }
+    return desligada;
+}
+
+function mostra_resultado(resul){
+    if(desligada) return;
+     document.getElementById("resultado").value = resul;
+}
+
+function operacao(nova_op){
+    if(desligada) return;
+    op = nova_op;
+    a = valor;
+    valor = "";
+    tem_ponto = false;
+}
+
+function calcula(){
+    if(desligada) return;
+    if(op != ""){
+        b = valor;
+        valor = "";
+        if(op == "soma") mostra_resultado(soma(a,b));
+        if(op == "sub") mostra_resultado(sub(a,b));
+        if(op == "div") mostra_resultado(div(a,b));
+        if(op == "mult") mostra_resultado(mult(a,b));
+        a = "";
+        b = "";
+        tem_ponto = false;
+
+    }
+}
+
+function digitando(tecla){
+    if(desligada) return;
+    if(tecla == "."){
+         if(!tem_ponto){
+            valor = valor + tecla;
+            mostra_resultado(valor);
+            tem_ponto = true;
+         }
+         return;
+    }
+    valor = valor + tecla;
+    mostra_resultado(valor);
+}
